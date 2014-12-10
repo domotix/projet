7dc389acb3c0ce00c04e468609dc33b44d05c8a3
a455bcd9 <kelemonde.aibo@club-internet.fr>
2014-12-10 19:48:02 +0100

    heroku modif database

--- db/migrate/20141208205701_create_devices.rb
+++ db/migrate/20141208205701_create_devices.rb
@@ -1,6 +1,7 @@
 class CreateDevices < ActiveRecord::Migration
   def change
   	# drop_table :devices
+  	# ligne supprimée à cause d'une erreur de migration ?
     create_table :devices do |t|
       t.string :name
       t.boolean :status
