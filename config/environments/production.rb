e8bfeb453f52af1caeee15babbb48b2040e20ade
a455bcd9 <kelemonde.aibo@club-internet.fr>
2014-12-10 14:17:34 +0100

    heroku modif migration devices

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ Projet::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  # config.assets.compile = false cf. post Eloi Zablocki
 
   # Generate digests for assets URLs.
   config.assets.digest = true
--- db/migrate/20141208205701_create_devices.rb
+++ db/migrate/20141208205701_create_devices.rb
@@ -1,6 +1,6 @@
 class CreateDevices < ActiveRecord::Migration
   def change
-  	drop_table :devices
+  	# drop_table :devices
     create_table :devices do |t|
       t.string :name
       t.boolean :status
