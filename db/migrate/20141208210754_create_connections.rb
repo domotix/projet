69b7f412fa1d3993329dfbe451c733f795d1a4b3
a455bcd9 <kelemonde.aibo@club-internet.fr>
2014-12-10 21:29:29 +0100

    modif create connections

--- db/migrate/20141208210754_create_connections.rb
+++ db/migrate/20141208210754_create_connections.rb
@@ -3,12 +3,14 @@ class CreateConnections < ActiveRecord::Migration
     create_table :connections do |t|
 
 # this line adds an integer column called `device_id` user_id.
-      t.connections :device, index: true
-      t.connections :user, index: true
+      #t.connections :device, index: true
+      #t.connections :user, index: true
+   #   t.connections :device
+   #   t.connections :user
 
       t.timestamps
     end
-    add_index :connections, :device_id
-    add_index :connections, :user_id
+    #add_index :connections, :device_id
+    #add_index :connections, :user_id
   end
 end
