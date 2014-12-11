bd3d9bb69343846c779865fabc51c98c1b637c5a
a455bcd9 <kelemonde.aibo@club-internet.fr>
2014-12-11 12:35:40 +0100

    modif controller connections et index

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -1,18 +1,62 @@
 class ConnectionsController < ApplicationController
-    # On ajoute la méthode connect dans la liste des méthodes où on set le device au début
-  before_action :set_connection, only: [:show, :edit, :update, :destroy, :add_user, :turn]
+    # On ajoute la méthode connect dans la liste des méthodes où on set le connection au début
+  before_action :set_connection, only: [:show, :edit, :update, :destroy]
 
   # On saute une etape de securite si on appelle connect en JSON
-  skip_before_action :verify_authenticity_token, only: [:connect, :add_user, :turn]
+  skip_before_action :verify_authenticity_token, only: [:connect]
 
-  # GET /devices
-  # GET /devices.json
+  # GET /connections
+  # GET /connections.json
   def index
     @connections = Connection.all
   end
 
-  # DELETE /devices/1
-  # DELETE /devices/1.json
+  # GET /connections/1
+  # GET /connections/1.json
+  def show
+  end
+
+  # GET /connections/new
+  def new
+    @connection = Connection.new
+  end
+
+  # GET /connection/1/edit
+  def edit
+  end
+
+  # POST /connections
+  # POST /connections.json
+  def create
+    @connection = Connection.new(connection_params)
+
+    respond_to do |format|
+      if @connection.save
+        format.html { redirect_to @connection, notice: 'Connection was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @connection }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @connection.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /connections/1
+  # PATCH/PUT /connections/1.json
+  def update
+    respond_to do |format|
+      if @connection.update(connection_params)
+        format.html { redirect_to @connection, notice: 'Connection was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @connection.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /connections/1
+  # DELETE /connections/1.json
   def destroy
     @connection.destroy
     respond_to do |format|
@@ -25,9 +69,9 @@ class ConnectionsController < ApplicationController
     @connection = Connection.find(params[:id])
   end
 
-  # GET /devices/1
-  # GET /devices/1.json
-  def show
-  end
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def connection_params
+      params.require(:connection).permit(:user_id, :device_id)
+    end
 
 end
